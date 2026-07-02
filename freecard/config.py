4c09c65b6dba22c78135690df6c59362a0b3e630
Carsten Müller <119843761+nosilam@users.noreply.github.com>
2026-07-02 07:43:05 +0200

    Create config.py

---
+++ freecard/config.py
@@ -0,0 +1,43 @@
+FreeCard Kids
+Configuration
+
+MIT License
+https://github.com/nosilam/FreeCard-Kids
+"""
+
+# ---------- Card ----------
+
+CARD_WIDTH = 85.0
+CARD_HEIGHT = 55.0
+
+BASE_HEIGHT = 1.8
+TOP_HEIGHT = 0.6
+
+CORNER_RADIUS = 5.0
+
+FRAME_WIDTH = 1.0
+
+# ---------- QR ----------
+
+QR_SIZE = 45.0
+QR_MARGIN = 2.0
+
+# ---------- Text ----------
+
+NAME_SIZE = 5.5
+
+PHONE_SIZE = 3.0
+
+INFO_SIZE = 2.2
+
+FONT = "Noto Sans"
+
+# ---------- Layout ----------
+
+LEFT_MARGIN = 5.0
+TOP_MARGIN = 5.0
+
+# ---------- Export ----------
+
+EXPORT_STL = True
+EXPORT_3MF = True
