9f9657c5a6200f33df832ee916f367ecc5eae3df
nosilam <malison+github@malison.org>
2026-07-28 15:43:28 +0200

    - create an svg and also an matrix for use in the makro - makro created as vcard_qr_matrix.py

--- create_vcard_qr_svg.py
+++ create_vcard_qr_svg.py
@@ -10,6 +10,11 @@ für direkten FreeCAD Import.
 """
 
 import segno
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_FILE = os.path.join(PROJECT_DIR, "vcard_qr.svg")
+MATRIX_FILE = os.path.join(PROJECT_DIR, "vcard_qr_matrix.py")
 
 from config import (
     CHILD_NAME,
@@ -19,9 +24,12 @@ from config import (
     QR_MARGIN
 )
 
+def create_qr(vcard):
 
-OUTPUT_FILE = "vcard_qr.svg"
-
+    return segno.make(
+        vcard,
+        error="q"
+    )
 
 # ==========================
 # Name zerlegen
@@ -55,10 +63,7 @@ END:VCARD
 # QR-Code erzeugen
 # ==========================
 
-QR = segno.make(
-    VCARD,
-    error="q"
-)
+QR = create_qr(VCARD)
 
 
 # ==========================
@@ -71,6 +76,35 @@ QR.save(
     border=QR_MARGIN
 )
 
+# ------------------------------------------------------------
+# Matrix für FreeCAD erzeugen
+# ------------------------------------------------------------
+
+MATRIX_FILE = os.path.join(PROJECT_DIR, "vcard_qr_matrix.py")
+
+with open(MATRIX_FILE, "w", encoding="utf-8") as f:
+
+    f.write('"""\n')
+    f.write("AUTOMATISCH ERZEUGT\n")
+    f.write("Nicht bearbeiten!\n")
+    f.write('"""\n\n')
+
+    matrix = list(QR.matrix)
+
+    f.write(f"QR_MODULES = {len(matrix)}\n\n")
+
+    f.write("QR_MATRIX = [\n")
+
+    for row in matrix:
+
+        line = ", ".join(
+            "True" if cell else "False"
+            for cell in row
+        )
+
+        f.write(f"    [{line}],\n")
+
+    f.write("]\n")
 
 # ==========================
 # Ausgabe
